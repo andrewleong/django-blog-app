2f38b16de41857fba4609e339e63ca895fef59eb
andrew leong <andrew.leong@jewelpaymentech.com>
2019-03-31 22:43:23 +0800

    Add mock data for posts

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+# Fake data
+posts = [
+    {
+        'author': 'Andrew',
+        'title': 'Blog post 1',
+        'content': 'First content here',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Slaine',
+        'title': 'Blog post 2',
+        'content': 'Second content here',
+        'date_posted': 'August 28, 2018'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
