5c5d7d63bcffa796d43cfc8b8a6d63a3fe2abfb3
andrew leong <andrew.leong@jewelpaymentech.com>
2019-05-26 14:52:51 +0800

    Add applications and routes

---
+++ blog_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/andrewleong/Documents/personal-projects/django-tutorial/blog_env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
